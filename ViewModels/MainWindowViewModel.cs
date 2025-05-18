5c1be32661735793ef07dff6ec2daa9d359c7222
Bartexpl98 <bartek.zmuda27@gmail.com>
2025-05-18 11:59:18 +0100

    Implemented individual person card

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -5,5 +5,5 @@ namespace FamilyTreeApp.ViewModels;
 public partial class MainWindowViewModel : ViewModelBase
 {
     //public string Greeting { get; } = "Welcome to Avalonia!";
-    public Person John { get; set; } = new Person("Gary", new DateTime(1980, 1, 1));
+    public Person Gary { get; set; } = new Person("Gary", new DateTime(1980, 1, 1));
 }
---
+++ Views/PersonCard.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace FamilyTreeApp.Views
+{
+    public partial class PersonCard : UserControl
+    {
+        public PersonCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
